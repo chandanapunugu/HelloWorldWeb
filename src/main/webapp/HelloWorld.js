35cf4e5c22c2f0bfaac2c107b2e05e21f39c8322
Chandra Punugu <pcsreddy@gmail.com>
2022-04-16 20:07:28 -0400

    My First Commit

---
+++ src/main/webapp/HelloWorld.js
@@ -0,0 +1,17 @@
+
+function sayHello( firstName, lastName) {
+	alert('Hello '+ firstName +' ' +lastName);
+}
+function cToF(celsius) {
+  var cTemp = celsius;
+  var cToFahr = cTemp * 9 / 5 + 32;
+  var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F.';
+    alert(message);
+}
+
+function fToC(fahrenheit){
+  var fTemp = fahrenheit;
+  var fToCel = (fTemp - 32) * 5 / 9;
+  var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+    alert(message);
+} 
